6ece0a9f477ab16432689b329f989fb3c1d6f1ba
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2020-03-24 13:14:31 -0700

    Add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { SongListComponent } from './song-list/song-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SongListComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     MatCardModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
