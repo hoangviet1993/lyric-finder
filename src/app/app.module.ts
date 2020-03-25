65ef736d6d078cd86be7717f92957d72e3281745
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2020-03-24 18:41:48 -0700

    Setup route for route navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,52 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SongListComponent } from './song-list/song-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LyricViewerComponent } from './lyric-viewer/lyric-viewer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/songs',
+    pathMatch: 'full',
+  },
+  {
+    path: 'lyrics/:artist/:title',
+    component: LyricViewerComponent
+  },
+  {
+    path: 'songs',
+    component: SongListComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SongListComponent,
     NavbarComponent,
+    LyricViewerComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
     MatCardModule,
     MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
